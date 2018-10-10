da2459aad9a825a4aa8df17076ed39ff6ea6a7ac
voytenkov <voytenkov-80@yandex.ru>
2018-10-10 18:31:03 +1000

    0.28

--- js/script.js
+++ js/script.js
@@ -2,19 +2,28 @@ var modalMap = document.querySelector(".modal-map");
 var modalLogin = document.querySelector(".modal-login");
 var modalMapButton = document.querySelector(".location-button");
 var modalContactButton = document.querySelector(".about-button");
-var modalClose = document.querySelector(".modal-close");
+var modalMapClose = document.querySelector(".modal-map .modal-close");
+var modalLoginClose = document.querySelector(".modal-login .modal-close");
 modalMapButton.addEventListener("click", function (evt) {
   "use strict";
   evt.preventDefault();
   modalMap.classList.add("modal-view");
 });
+
 modalContactButton.addEventListener("click", function (evt) {
   "use strict";
   evt.preventDefault();
   modalLogin.classList.add("modal-view");
 });
-modalClose.addEventListener("click", function (evt) {
+
+modalMapClose.addEventListener("click", function (evt) {
   "use strict";
   evt.preventDefault();
   modalMap.classList.remove("modal-view");
+});
+
+modalLoginClose.addEventListener("click", function (evt) {
+  "use strict";
+  evt.preventDefault();
+  modalLogin.classList.remove("modal-view");
 });
